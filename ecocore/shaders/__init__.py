2dc7b0e0c250a4d963a1814afc5e940bfc8b0fb4
noahpyne1 <noahpyne10@gmail.com>
2024-12-28 00:07:28 -0800

    Add files via upload

---
+++ ecocore/shaders/__init__.py
@@ -0,0 +1,11 @@
+from ecocore.shaders.normals import normals_shader
+from ecocore.shaders.basic_lighting import basic_lighting_shader
+from ecocore.shaders.triplanar import triplanar_shader
+
+
+
+
+# post processing effects
+from ecocore.shaders.camera_vertical_blur import camera_vertical_blur_shader
+from ecocore.shaders.camera_contrast import camera_contrast_shader
+from ecocore.shaders.camera_grayscale import camera_grayscale_shader
