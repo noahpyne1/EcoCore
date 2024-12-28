6323545dde9ffa3daab3e40a591765582879158d
noahpyne1 <noahpyne10@gmail.com>
2024-12-27 22:37:19 -0800

    Delete events.py

--- events.py
+++
@@ -1,8 +0,0 @@
-import sdl2
-from .core import Game
-
-def poll():
-    events = sdl2.ext.get_events()
-    for event in events:
-        if event.type == sdl2.SDL_QUIT:
-            Game.running = False
